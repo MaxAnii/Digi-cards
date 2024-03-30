11016e5c08ad94e88f07fdff4c21fd4b36fd41d2
MaxAnii <ansarbaba2000@gmail.com>
2024-03-30 20:40:58 +0530

    Added db via prisma.

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+// development mode so that in hot reload it does not create any new prisma client
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+// production  mode
+// export const db =  new PrismaClient();
