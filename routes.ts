8450676ed64fc1e6b198dedda066ffdb639b6286
MaxAnii <ansarbaba2000@gmail.com>
2024-03-30 20:42:32 +0530

    Added api folder and routes for the next auth.

---
+++ routes.ts
@@ -0,0 +1,7 @@
+export const publicRoutes = ["/user"];
+
+export const authRoutes = ["/", "/signin", "/signup", "/emailverification"];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/user";
