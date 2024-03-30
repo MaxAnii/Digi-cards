5bbe6be87ecb5975383762579dccd60b6aa292dd
MaxAnii <ansarbaba2000@gmail.com>
2024-03-30 20:43:19 +0530

    Added schema for login and signup.

---
+++ schemas/index.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const loginSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+});
+
+export const signupSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+	confrimPassword: z.string().min(8),
+});
