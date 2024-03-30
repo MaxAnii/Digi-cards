7bdc0eacb68dcc4936935ffe27e28300203c0534
MaxAnii <ansarbaba2000@gmail.com>
2024-03-30 20:39:39 +0530

    Function for signup.

--- actions/signup.ts
+++ actions/signup.ts
@@ -1,3 +1,35 @@
+"use server";
 import { signupSchema } from "@/schemas";
 import * as z from "zod";
-export const signup = async (values: z.infer<typeof signupSchema>) => {};
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+// import { generateVerificationToken } from "@/lib/tokens";
+// import { sendVerificatonEmail } from "@/lib/mail";
+export const signup = async (values: z.infer<typeof signupSchema>) => {
+	const validateFields = signupSchema.safeParse(values);
+	if (!validateFields.success) return { error: "error" };
+
+	const { email, password } = validateFields.data;
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const existUser = await db.user.findUnique({
+		where: {
+			email,
+		},
+	});
+	if (existUser) return { error: "Email already in use" };
+
+	await db.user.create({
+		data: {
+			email,
+			password: hashedPassword,
+		},
+	});
+	//   const verificationToken = await generateVerificationToken(email);
+	//   if (verificationToken.email) {
+	//     await sendVerificatonEmail(
+	//       verificationToken.email,
+	//       verificationToken.token,
+	//     );
+	//   }
+	return { success: "auth" };
+};
