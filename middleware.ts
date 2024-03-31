2a522f7342cfd2a802be840ff264cfff244fedd8
MaxAnii <ansarbaba2000@gmail.com>
2024-03-31 23:36:25 +0530

    Remove the public routes from middleware.

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,7 @@ export default auth((req) => {
 	const { nextUrl } = req;
 	const isLoggedIn = !!req.auth;
 	const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-	const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+	// const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 	const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 	if (isApiAuthRoute) {
 		return;
@@ -23,10 +23,6 @@ export default auth((req) => {
 			return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
 		}
 		return;
-	}
-
-	if (!isLoggedIn && !isPublicRoute) {
-		return Response.redirect(new URL("/signin", nextUrl));
 	}
 	return;
 });
