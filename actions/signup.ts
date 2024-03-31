05c7ca8f22297ea5828c8048ce239a4b3b2a954a
MaxAnii <ansarbaba2000@gmail.com>
2024-03-31 23:39:42 +0530

    Added sign up functionality.

--- actions/signup.ts
+++ actions/signup.ts
@@ -3,33 +3,39 @@ import { signupSchema } from "@/schemas";
 import * as z from "zod";
 import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
-// import { generateVerificationToken } from "@/lib/tokens";
-// import { sendVerificatonEmail } from "@/lib/mail";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificatonEmail } from "@/lib/mail";
+import { checkUsernameExists } from "./checkUsername";
 export const signup = async (values: z.infer<typeof signupSchema>) => {
 	const validateFields = signupSchema.safeParse(values);
-	if (!validateFields.success) return { error: "error" };
+	if (!validateFields.success) return { message: "Invalid fields" };
 
-	const { email, password } = validateFields.data;
+	const { email, password, username } = validateFields.data;
 	const hashedPassword = await bcrypt.hash(password, 10);
+	const usernameExists = await checkUsernameExists(username);
+
+	if (usernameExists) return { message: "User name already exists" };
 	const existUser = await db.user.findUnique({
 		where: {
 			email,
 		},
 	});
-	if (existUser) return { error: "Email already in use" };
+	if (existUser) return { message: "Email already in use" };
 
 	await db.user.create({
 		data: {
+			username,
 			email,
 			password: hashedPassword,
+			role: "user",
 		},
 	});
-	//   const verificationToken = await generateVerificationToken(email);
-	//   if (verificationToken.email) {
-	//     await sendVerificatonEmail(
-	//       verificationToken.email,
-	//       verificationToken.token,
-	//     );
-	//   }
-	return { success: "auth" };
+	const verificationToken = await generateVerificationToken(email);
+	if (verificationToken.email) {
+		await sendVerificatonEmail(
+			verificationToken.email,
+			verificationToken.token
+		);
+	}
+	return { message: "Verification email sent" };
 };
