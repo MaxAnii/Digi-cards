190ed9da97f41681491c576913deff775448c89c
MaxAnii <ansarbaba2000@gmail.com>
2024-03-31 23:49:08 +0530

    show edit button if user logged in.

--- components/EditProfileButton.tsx
+++ components/EditProfileButton.tsx
@@ -1,10 +1,20 @@
+import { useCurrentUser } from "@/hook/CurrentUserSession";
 import { Card } from "./ui/card";
 import { FiEdit } from "react-icons/fi";
+import { useParams } from "next/navigation";
 const EditProfileButton = () => {
+	const params = useParams();
+	const user = useCurrentUser();
 	return (
-		<Card className="md:h-[80px] md:w-[80px] h-[80px] w-[70px] mt-10  md:mx-5 p-5 mx-3 dark:bg-slate-300 dark:text-black">
-			<FiEdit size="3-xl" />
-		</Card>
+		<>
+			{user?.role === "user" && user?.username === params.username ? (
+				<Card className="md:h-[80px] md:w-[80px] h-[80px] w-[70px] mt-10  md:mx-5 p-5 mx-3 dark:bg-slate-300 dark:text-black">
+					<FiEdit size="3-xl" />
+				</Card>
+			) : (
+				<></>
+			)}
+		</>
 	);
 };
 
