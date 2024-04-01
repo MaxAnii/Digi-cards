d0e42d6c6d33923babd527060b7ba8c51e2d5e4d
MaxAnii <ansarbaba2000@gmail.com>
2024-04-01 23:50:21 +0530

    User data container.

---
+++ components/UserData.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { UserListContext } from "@/hook/UserListContext";
+import ListUserTable from "@/components/ListUserTable";
+import { Triangle } from "react-loader-spinner";
+const UserData = () => {
+	const groupList = useContext(UserListContext);
+	return (
+		<div className=" my-4 h-[80vh]  p-2 md:mx-[15vw]  md:p-4">
+			{!groupList.gettingUserList ? (
+				<ListUserTable></ListUserTable>
+			) : (
+				<div className="flex h-[80vh] items-center justify-center">
+					<Triangle
+						height="80"
+						width="80"
+						color="#4fa94d"
+						ariaLabel="triangle-loading"
+					/>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default UserData;
