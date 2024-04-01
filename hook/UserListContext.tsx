dd0ed0d11958b932ac9f142e032f6fb8ca3e25dc
MaxAnii <ansarbaba2000@gmail.com>
2024-04-01 23:45:30 +0530

    created context api for the user details.

---
+++ hook/UserListContext.tsx
@@ -0,0 +1,58 @@
+"use client";
+import {
+	createContext,
+	useState,
+	useEffect,
+	ReactNode,
+	useTransition,
+} from "react";
+import { getUsersList } from "@/actions/user";
+
+export type userData = {
+	id: string;
+	username: string;
+	name: string;
+	email: string;
+	image: string | null;
+};
+
+type userDataType = userData[] | undefined;
+
+type UserListContextType = {
+	data: userDataType;
+	setCallFunction: React.Dispatch<React.SetStateAction<boolean>>;
+	gettingUserList: Boolean;
+};
+
+export const UserListContext = createContext<UserListContextType>({
+	data: undefined,
+	setCallFunction: () => {},
+	gettingUserList: false,
+});
+
+const UserListProvider = ({ children }: { children: ReactNode }) => {
+	const [gettingUserList, startTransition] = useTransition();
+	const [data, setData] = useState<userDataType>();
+	const [callFunction, setCallFunction] = useState<boolean>(false);
+
+	const getList = async () => {
+		const userList = await getUsersList();
+		setData(userList);
+	};
+
+	useEffect(() => {
+		startTransition(async () => {
+			await getList();
+		});
+	}, [callFunction]);
+
+	return (
+		<UserListContext.Provider
+			value={{ data, setCallFunction, gettingUserList }}
+		>
+			{children}
+		</UserListContext.Provider>
+	);
+};
+
+export default UserListProvider;
