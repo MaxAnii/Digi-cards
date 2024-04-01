598e1d45a0bf9f9b2b5bdf4ecfca905ec6db493c
MaxAnii <ansarbaba2000@gmail.com>
2024-04-01 23:44:10 +0530

    Added page for admin.

---
+++ app/(protectedRoutes)/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import AdminNavbarLinks from "@/components/AdminNavbarLinks";
+import AuthButtons from "@/components/AuthButtons";
+import NavbarContainer from "@/components/NavbarContainer";
+import UserListProvider from "@/hook/UserListContext";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div>
+			<UserListProvider>
+				<NavbarContainer>
+					<AdminNavbarLinks></AdminNavbarLinks>
+					<AuthButtons></AuthButtons>
+				</NavbarContainer>
+				{children}
+			</UserListProvider>
+		</div>
+	);
+};
+
+export default layout;
