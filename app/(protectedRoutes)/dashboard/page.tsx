a1c6e55d5761ca462af03061785364d084967dfb
MaxAnii <ansarbaba2000@gmail.com>
2024-04-01 23:45:53 +0530

    Added page for admin.

---
+++ app/(protectedRoutes)/dashboard/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import UserData from "@/components/UserData";
+
+const Page = () => {
+	return (
+		<div className="pl-2 md:pl-9  pt-[100px]">
+			<div className="w-full max-w-[90vw] md:flex ">
+				<div>
+					<div className="pb-2 md:text-3xl text-xl">User List</div>
+					<p className="md:text-xl text-sm">
+						Here you can see and remove users form your system{" "}
+					</p>
+				</div>
+				<div className="ml-auto sm:mt-3 md:mt-0"></div>
+			</div>
+			<UserData></UserData>
+		</div>
+	);
+};
+
+export default Page;
