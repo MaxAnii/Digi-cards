f5440a66ae0fd4b9ce35502b91f7e0676a3f3acf
MaxAnii <ansarbaba2000@gmail.com>
2024-04-02 23:20:18 +0530

    Added a resetSchema & resetPasswordSchema.

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,11 @@
 import * as z from "zod";
 
+export const resetSchema = z.object({
+	email: z.string().email(),
+});
+export const resetPasswordSchema = z.object({
+	password: z.string().min(8),
+});
 export const loginSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(8),
