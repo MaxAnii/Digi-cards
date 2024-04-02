e8d1e9d3eee5ad9683b3b34733597145f83b634a
MaxAnii <ansarbaba2000@gmail.com>
2024-04-02 23:14:52 +0530

    Fetch icon dynamically instead of static images.

--- components/ContactIcon.tsx
+++ components/ContactIcon.tsx
@@ -14,39 +14,73 @@ import phone from "@/public/phone.png";
 
 import Image from "next/image";
 const ContactIcon = () => {
-	const socialIcons = {
+	const socialIcons = [
 		phone,
 		whatsapp,
-		facebook,
-		github,
-		gmail,
-		instagram,
-		linkedin,
-		pinterest,
-		snapchat,
+		// 	facebook,
+		// 	github,
+		// 	gmail,
+		// 	instagram,
+		// 	linkedin,
+		// 	pinterest,
+		// 	snapchat,
 		telegram,
-		twitter,
-		youtube,
-	};
+		// 	twitter,
+		// 	youtube,
+	];
+	const socialMedia = [
+		"https://www.facebook.com/fakeuser123",
+		"https://www.instagram.com/fakeuser123",
+		"https://www.linkedin.com/in/fakeuser123",
+		"https://www.tiktok.com/@fakeuser123",
+		"https://www.youtube.com/user/fakeuser123",
+		"https://twitter.com/fakeuser123",
+		"https://www.pinterest.com/fakeuser123",
+		"https://www.snapchat.com/add/fakeuser123",
+		"https://www.reddit.com/user/fakeuser123",
+		"https://itsmeansar.vercel.app/",
+	];
 
 	return (
 		<>
-			{Object.entries(socialIcons).map(([name, value], index) => {
+			{/* {Object.entries(socialIcons).map(([name, value], index) => { */}
+			{socialIcons.map((elem, index) => {
+				// const link = new URL(elem).hostname;
 				return (
-					<Card
-						key={index}
-						className="p-5 dark:bg-slate-700 md:h-[140px] md:w-[140px] h-[120px] w-[120px] hover:rotate-12"
-					>
-						<Image
-							className=" md:h-[70px] md:w-[70px] h-[50px] w-[50px]  rounded-full border-4 border-[#f7f7f7] dark:dark:bg-slate-300"
-							// src="https://special-harlequin-squid.faviconkit.com/itsmeansar.vercel.app/356"
-							src={value}
-							alt="favicon"
-						></Image>
-						<CardDescription className="dark:text-white md:pt-3 pt-1 text-center">
-							{name}
-						</CardDescription>
-					</Card>
+					<a href={elem} target="_blank">
+						<Card
+							key={index}
+							className="p-5  md:h-[140px] md:w-[140px] h-[120px] w-[120px] hover:translate-y-2 dark:bg-slate-200 "
+						>
+							<Image
+								className=" md:h-[70px] md:w-[70px] h-[50px] w-[50px]  rounded-xl   p-2 "
+								// src={`https://special-harlequin-squid.faviconkit.com/${link}/356`}
+								src={elem}
+								alt="favicon"
+							></Image>
+							<CardDescription className="dark:text-black md:pt-3 pt-1 text-center"></CardDescription>
+						</Card>
+					</a>
+				);
+			})}
+			{socialMedia.map((elem, index) => {
+				const link = new URL(elem).hostname;
+				return (
+					<a href={elem} target="_blank">
+						<Card
+							key={index}
+							className="p-5  md:h-[140px] md:w-[140px] h-[120px] w-[120px] hover:translate-y-2 dark:bg-slate-200 "
+						>
+							<img
+								className=" md:h-[70px] md:w-[70px] h-[50px] w-[50px]  rounded-xl   p-2 "
+								src={`https://special-harlequin-squid.faviconkit.com/${link}/356`}
+								alt="favicon"
+							></img>
+							<CardDescription className="dark:text-black md:pt-3 pt-1 text-center">
+								{`@${link.slice(4, 15)}`}
+							</CardDescription>
+						</Card>
+					</a>
 				);
 			})}
 		</>
