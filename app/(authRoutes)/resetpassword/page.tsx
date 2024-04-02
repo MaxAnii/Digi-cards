0fef88d7bc77d11e7ae6d39b1909b77ca4d69fe4
MaxAnii <ansarbaba2000@gmail.com>
2024-04-02 23:14:03 +0530

    Added page to reset password

---
+++ app/(authRoutes)/resetpassword/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import FormContainer from "@/components/auth/FormContainer";
+import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
+
+const ResetPassword = () => {
+	return (
+		<>
+			<div className="flex justify-center items-center h-[80vh]">
+				<FormContainer
+					title="Reset Password"
+					backButtonLabel="Back to login"
+					backButtonhref="/signin"
+				>
+					<ResetPasswordForm></ResetPasswordForm>
+				</FormContainer>
+			</div>
+		</>
+	);
+};
+
+export default ResetPassword;
