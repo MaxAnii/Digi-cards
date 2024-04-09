423e356da514e2496fc058dc0f7fdb964f1b9c86
MaxAnii <ansarbaba2000@gmail.com>
2024-04-09 17:44:57 +0530

    Don't navigate after logout

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -3,11 +3,8 @@ import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
 import { useCurrentUser } from "@/hook/CurrentUserSession";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-
 const AuthButtons = () => {
 	const user = useCurrentUser();
-	const router = useRouter();
 	return (
 		<>
 			{user ? (
@@ -15,7 +12,6 @@ const AuthButtons = () => {
 					className="pl-2  text-sm"
 					onClick={() => {
 						signOut();
-						router.push("/");
 					}}
 				>
 					Logout
