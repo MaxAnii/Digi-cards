cae406f8216f97faf399cb160a9ab44323ee7ccc
MaxAnii <ansarbaba2000@gmail.com>
2024-04-09 15:31:53 +0530

    Added script to generate prisma db

--- lib/db.ts
+++ lib/db.ts
@@ -3,9 +3,9 @@ declare global {
 	var prisma: PrismaClient | undefined;
 }
 // development mode so that in hot reload it does not create any new prisma client
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
 // production  mode
-// export const db =  new PrismaClient();
+export const db = new PrismaClient();
