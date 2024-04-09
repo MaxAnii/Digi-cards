f123e463a16e5975c58c7d3a45490968fd31f8bd
MaxAnii <ansarbaba2000@gmail.com>
2024-04-09 17:41:31 +0530

    add a responsive website.

--- components/NavbarContainer.tsx
+++ components/NavbarContainer.tsx
@@ -1,13 +1,18 @@
+import Image from "next/image";
+
 import Link from "next/link";
 const NavbarContainer = ({ children }: { children: React.ReactNode }) => {
 	return (
 		// <div className="p-6 flex fixed top-0 left-0 right-0 z-10   bg-white/40 shadow-lg ring-1 ring-black/5">
 		<div>
-			<div className="p-6 flex fixed top-0 left-0 right-0 z-10  bg-white/10 shadow-xl backdrop-blur-lg  ">
-				<Link href="/" className="md:text-3xl text-lg font-mono text-gray-100">
+			<div className="p-6 md:flex fixed top-0 left-0 right-0 z-10  bg-white/10 shadow-xl backdrop-blur-lg  ">
+				<Link
+					href="/"
+					className="flex md:text-3xl text-lg font-mono text-gray-100"
+				>
 					Digi-Cards
 				</Link>
-				<div className="ml-auto mr-2 flex">{children}</div>
+				<div className="md:ml-auto mr-2 flex justify-end">{children}</div>
 			</div>
 			<div className="p-6 flex fixed top-0 left-0 right-0 z-0 bg-gradient-to-r from-black to-white "></div>
 		</div>
