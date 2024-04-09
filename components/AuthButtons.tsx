a62baf3e48a8befa32b1f796066319d683449dab
MaxAnii <ansarbaba2000@gmail.com>
2024-04-09 17:30:47 +0530

    redirect to landing page after logout

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -3,13 +3,21 @@ import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
 import { useCurrentUser } from "@/hook/CurrentUserSession";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const AuthButtons = () => {
 	const user = useCurrentUser();
+	const router = useRouter();
 	return (
 		<>
 			{user ? (
-				<Button className="pl-2  text-sm" onClick={() => signOut()}>
+				<Button
+					className="pl-2  text-sm"
+					onClick={() => {
+						signOut();
+						router.push("/");
+					}}
+				>
 					Logout
 				</Button>
 			) : (
