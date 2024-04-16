53d2b3392be393216d1c0de50cb807ea2cb118ec
MaxAnii <ansarbaba2000@gmail.com>
2024-04-16 10:32:30 +0530

    remove imaged.

--- components/UserProfilePhotoContainer.tsx
+++ components/UserProfilePhotoContainer.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useContext, useEffect, useState, useTransition } from "react";
 import { CardDescription, CardHeader, CardTitle } from "./ui/card";
-import UserProfilePhoto from "./UserProfilePhoto";
-import BackgroundProfilePhoto from "./BackgroundProfilePhoto";
+// import UserProfilePhoto from "./UserProfilePhoto";
+// import BackgroundProfilePhoto from "./BackgroundProfilePhoto";
 import { UserInformationContext } from "@/hook/userInformationContext";
 import { getNameDescription } from "@/actions/userInformation";
 import NameDescriptionSkeletonLoader from "./NameDescriptionSkeletonLoader";
@@ -26,16 +26,12 @@ const UserProfilePhotoContainer = () => {
 	}, [userInformation.callNameDescription]);
 	return (
 		<div className=" lg:max-w-[500px] m-1 ">
-			<BackgroundProfilePhoto></BackgroundProfilePhoto>
-			<UserProfilePhoto></UserProfilePhoto>
+			{/* <BackgroundProfilePhoto></BackgroundProfilePhoto>
+			<UserProfilePhoto></UserProfilePhoto> */}
 			{!isPending ? (
 				<CardHeader className="mt-[-40px]">
-					<CardTitle className=" text-3xl">
-						{data?.name || "Your Name"}
-					</CardTitle>
-					<CardDescription className="mb-1">
-						{data?.bio || "Small description of yourself"}
-					</CardDescription>
+					<CardTitle className=" text-3xl">{data?.name}</CardTitle>
+					<CardDescription className="mb-1">{data?.bio}</CardDescription>
 				</CardHeader>
 			) : (
 				<NameDescriptionSkeletonLoader></NameDescriptionSkeletonLoader>
