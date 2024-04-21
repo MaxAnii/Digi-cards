fd55001bbd537b5754e3b9c6b5b43949a4ced90b
MaxAnii <ansarbaba2000@gmail.com>
2024-04-21 15:44:01 +0530

    Don't send images.

--- actions/userInformation.ts
+++ actions/userInformation.ts
@@ -85,7 +85,17 @@ export const getAllInformation = async (id: string) => {
 		const data = await db.user.findFirst({
 			where: { id },
 			select: {
-				BasicDetails: true,
+				BasicDetails: {
+					select: {
+						email: true,
+						whatsapp: true,
+						whatsappCountryCode: true,
+						phone: true,
+						phoneCountryCode: true,
+						name: true,
+						bio: true,
+					},
+				},
 				socialLinks: true,
 			},
 		});
