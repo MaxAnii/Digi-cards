9fd9274203160623bc5b79def1d94e35470c75b4
MaxAnii <ansarbaba2000@gmail.com>
2024-06-01 22:44:06 +0530

    style: change the navbar bg color.

--- components/NavbarContainer.tsx
+++ components/NavbarContainer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 const NavbarContainer = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div>
-			<div className="md:p-6 p-2 py-6 flex shadow-xl  bg-[#ebedee]">
+			<div className="md:p-6 p-2 py-6 flex shadow-xl  bg-[#c2c3c3e3]">
 				<Link href="/" className=" md:text-3xl text-xl font-mono ">
 					DigiCards
 				</Link>
