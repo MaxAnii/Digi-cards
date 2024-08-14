debd219024608f9e1156fd001b58dc5f8a8f35a4
MaxAnii <ansarbaba2000@gmail.com>
2024-08-14 18:37:33 +0530

    style: creates new blinkIcon component.

---
+++ components/BlinkingIcons.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+import {
+	FaFacebook,
+	FaTwitter,
+	FaInstagram,
+	FaLinkedin,
+	FaYoutube,
+	FaPinterest,
+	FaSnapchat,
+	FaReddit,
+	FaTumblr,
+	FaFlickr,
+	FaTwitch,
+	FaWhatsapp,
+	FaVimeo,
+	FaSkype,
+	FaSlack,
+	FaDribbble,
+	FaBehance,
+	FaMedium,
+	FaGithub,
+	FaGitlab,
+	FaStackOverflow,
+	FaStackExchange,
+	FaTelegram,
+	FaDiscord,
+	FaWeibo,
+	FaQuora,
+	FaWeixin,
+	FaRss,
+} from "react-icons/fa";
+import { Card } from "./ui/card";
+
+const BlinkingIcons = () => {
+	return (
+		<Card className="relative overflow-hidden ml-9 w-[500px] pr-5 h-screen icon-background hidden md:block">
+			<div className="blinking-icon icon1">
+				<FaFacebook />
+			</div>
+			<div className="blinking-icon icon2">
+				<FaTwitter />
+			</div>
+			<div className="blinking-icon icon3">
+				<FaInstagram />
+			</div>
+			<div className="blinking-icon icon4">
+				<FaLinkedin />
+			</div>
+			<div className="blinking-icon icon5">
+				<FaYoutube />
+			</div>
+			<div className="blinking-icon icon6">
+				<FaPinterest />
+			</div>
+			<div className="blinking-icon icon7">
+				<FaSnapchat />
+			</div>
+			<div className="blinking-icon icon8">
+				<FaReddit />
+			</div>
+			<div className="blinking-icon icon9">
+				<FaTumblr />
+			</div>
+			<div className="blinking-icon icon10">
+				<FaFlickr />
+			</div>
+			<div className="blinking-icon icon11">
+				<FaTwitch />
+			</div>
+			<div className="blinking-icon icon12">
+				<FaWhatsapp />
+			</div>
+			<div className="blinking-icon icon13">
+				<FaVimeo />
+			</div>
+			<div className="blinking-icon icon14">
+				<FaSkype />
+			</div>
+			<div className="blinking-icon icon15">
+				<FaSlack />
+			</div>
+			<div className="blinking-icon icon16">
+				<FaDribbble />
+			</div>
+			<div className="blinking-icon icon17">
+				<FaBehance />
+			</div>
+			<div className="blinking-icon icon18">
+				<FaMedium />
+			</div>
+			<div className="blinking-icon icon19">
+				<FaGithub />
+			</div>
+			<div className="blinking-icon icon20">
+				<FaGitlab />
+			</div>
+			<div className="blinking-icon icon21">
+				<FaStackOverflow />
+			</div>
+			<div className="blinking-icon icon22">
+				<FaStackExchange />
+			</div>
+			<div className="blinking-icon icon23">
+				<FaTelegram />
+			</div>
+			<div className="blinking-icon icon24">
+				<FaDiscord />
+			</div>
+			<div className="blinking-icon icon25">
+				<FaWeibo />
+			</div>
+			<div className="blinking-icon icon26">
+				<FaQuora />
+			</div>
+			<div className="blinking-icon icon27">
+				<FaWeixin />
+			</div>
+			<div className="blinking-icon icon28">
+				<FaRss />
+			</div>
+		</Card>
+	);
+};
+
+export default BlinkingIcons;
