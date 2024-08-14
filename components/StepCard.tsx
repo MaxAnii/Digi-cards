f64ab32e36d86d2f685ad5127e9cc4d2086d6ff9
MaxAnii <ansarbaba2000@gmail.com>
2024-08-14 19:05:22 +0530

    feat; adds a component to show how it work.

---
+++ components/StepCard.tsx
@@ -0,0 +1,23 @@
+const StepCard = () => {
+	return (
+		<main>
+			<ol className="gradient-list">
+				<li>Register by creating your unique URL.</li>
+				<li>Verify your email by clicking the link sent to your email.</li>
+				<li>Add your name and bio.</li>
+				<li>Upload your profile photo and banner photo.</li>
+				<li>
+					Add contact methods such as your phone number, WhatsApp number, and
+					social media links.
+				</li>
+				<li>
+					Share your profile URL with others to allow them to contact you
+					through any of the provided options.
+				</li>
+				<li>Edit your links and phone numbers as needed.</li>
+			</ol>
+		</main>
+	);
+};
+
+export default StepCard;
