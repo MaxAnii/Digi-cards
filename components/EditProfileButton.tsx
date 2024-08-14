881d36a8858b9305fded2ea67b39c72c50939352
MaxAnii <ansarbaba2000@gmail.com>
2024-08-14 18:39:44 +0530

    style: changes the background color.

--- components/EditProfileButton.tsx
+++ components/EditProfileButton.tsx
@@ -8,7 +8,7 @@ const EditProfileButton = () => {
 	return (
 		<>
 			{user?.username! == params.username ? (
-				<Card className="md:h-[50px] md:w-[50px] h-[50px] w-[50px]   p-3  dark:bg-slate-300 dark:text-black hover:translate-y-2">
+				<Card className="md:h-[50px] md:w-[50px] h-[50px] w-[50px] bg-black text-white   p-3   hover:translate-y-2">
 					<FiEdit size="2-xl" />
 				</Card>
 			) : (
