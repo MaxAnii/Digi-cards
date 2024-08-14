de64e21af862ed3bbf435a28463ec9def12c01d7
MaxAnii <ansarbaba2000@gmail.com>
2024-08-14 18:34:04 +0530

    style: creats new landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,16 @@
-import React from "react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import NavbarContainer from "@/components/NavbarContainer";
+import HeroSection from "@/components/HeroSection";
+import DescriptionSection from "@/components/DescriptionSection";
+import Footer from "@/components/Footer";
 const LandingPage = () => {
 	return (
 		<>
 			<NavbarContainer>
 				<></>
 			</NavbarContainer>
-			<div className="flex h-screen w-full items-center justify-center">
-				<div className="text-center">
-					<Button className="my-2 rounded-3xl p-5 text-xl " variant="outline">
-						Welcome to Digi Cards
-					</Button>
-					<h1 className="text-3xl font-bold lg:text-5xl">
-						Create & Share Your Digital Profile
-					</h1>
-					<p className="w-full p-4 text-center lg:px-[120px]">
-						Digi-Cards provides a powerful solution for crafting and sharing
-						digital business cards. With our platform, you can easily create
-						professional online business cards and share them with others.
-						Enhance your networking and establish your digital presence with
-						Digi-Cards.
-					</p>
-					<div className="text-3xl ">
-						<Link href="/signin">
-							<Button size="lg" className="text-base ">
-								Get Started
-							</Button>
-						</Link>
-					</div>
-				</div>
-			</div>
+			<HeroSection></HeroSection>
+			<DescriptionSection></DescriptionSection>
+			<Footer></Footer>
 		</>
 	);
 };
