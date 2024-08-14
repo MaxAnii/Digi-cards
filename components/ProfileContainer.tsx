57bb424a5a1b01d0fba1d2d6d61db405b6a374a0
MaxAnii <ansarbaba2000@gmail.com>
2024-08-14 18:43:03 +0530

    refactor: removing comments

--- components/ProfileContainer.tsx
+++ components/ProfileContainer.tsx
@@ -8,7 +8,6 @@ const ProfileContainer = () => {
 	return (
 		<>
 			<div className="flex justify-center profile-background min-h-[95vh] ">
-				{/* <div className="flex justify-center bg-gradient-to-r from-slate-300 to-slate-500 min-h-[95vh] "> */}
 				<div className="  my-5  rounded-lg  pb-5 ">
 					<div className="">
 						<UserProfilePhotoContainer></UserProfilePhotoContainer>
