e0be7839417924b68403bdc253e7f1987d200015
MaxAnii <ansarbaba2000@gmail.com>
2024-08-17 21:28:12 +0530

    style: add animation on scroll.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
 import NavbarContainer from "@/components/NavbarContainer";
 import HeroSection from "@/components/HeroSection";
 import DescriptionSection from "@/components/DescriptionSection";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const LandingPage = () => {
+	useEffect(() => {
+		AOS.init({
+			duration: 1200,
+			once: true,
+		});
+	});
 	return (
 		<>
 			<NavbarContainer>
--- components/StepCard.tsx
+++ components/StepCard.tsx
@@ -2,19 +2,21 @@ const StepCard = () => {
 	return (
 		<main>
 			<ol className="gradient-list">
-				<li>Register by creating your unique URL.</li>
-				<li>Verify your email by clicking the link sent to your email.</li>
-				<li>Add your name and bio.</li>
-				<li>Upload your profile photo and banner photo.</li>
-				<li>
+				<li data-aos="fade-up">Register by creating your unique URL.</li>
+				<li data-aos="fade-up">
+					Verify your email by clicking the link sent to your email.
+				</li>
+				<li data-aos="fade-up">Add your name and bio.</li>
+				<li data-aos="fade-up">Upload your profile photo and banner photo.</li>
+				<li data-aos="fade-up">
 					Add contact methods such as your phone number, WhatsApp number, and
 					social media links.
 				</li>
-				<li>
+				<li data-aos="fade-up">
 					Share your profile URL with others to allow them to contact you
 					through any of the provided options.
 				</li>
-				<li>Edit your links and phone numbers as needed.</li>
+				<li data-aos="fade-up">Edit your links and phone numbers as needed.</li>
 			</ol>
 		</main>
 	);
